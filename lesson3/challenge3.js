bfd097b82e34cbb6acfae5ec3a08457070dca1ac
Hotown <hotown0515@163.com>
2017-09-19 22:23:22 +0800

    edit README.md

--- lesson3/challenge3.js
+++ lesson3/challenge3.js
@@ -8,25 +8,29 @@ const superagent = require("superagent");
 const app = express();
 
 app.get("/", function(req, res, next) {
-    superagent.get("https://cnodejs.org/").end(function(err, sres) {
-      if (err) {
-        return next(err);
-      }
-      const $ = cheerio.load(sres.text);
-      const items = [];
-      $("#topic_list .cell").each(function(idx, element) {
-        const $element = $(element);
-        items.push({ title: $element
-            .children(".topic_title_wrapper")
-            .children(".topic_title")
-            .attr("title"), href: $element
-            .children(".topic_title_wrapper")
-            .children(".topic_title")
-            .attr("href"), author: $element
-            .children(".user_avatar")
-            .children()
-            .attr("title") });
+  superagent.get("https://cnodejs.org/").end(function(err, sres) {
+    if (err) {
+      return next(err);
+    }
+    const $ = cheerio.load(sres.text);
+    const items = [];
+    $("#topic_list .cell").each(function(idx, element) {
+      const $element = $(element);
+      items.push({
+        title: $element
+          .children(".topic_title_wrapper")
+          .children(".topic_title")
+          .attr("title"),
+        href: $element
+          .children(".topic_title_wrapper")
+          .children(".topic_title")
+          .attr("href"),
+        author: $element
+          .children(".user_avatar")
+          .children()
+          .attr("title")
       });
-      res.send(items);
     });
+    res.send(items);
+  });
 });
